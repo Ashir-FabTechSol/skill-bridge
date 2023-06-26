ac8697d271450baf55891cc43cc648eecb6cf773
Ashir-FabTechSol <ashirfabtechsol@gmail.com>
2023-06-26 15:02:59 +0500

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,398 @@
+// $('.autoplay-2').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     dots: false, // add dots option
+//     arrows: true,
+//     prevArrow:'.priv_arrow',
+//     nextArrow:'.next_arrow',
+//     responsive: [
+//       {
+//         breakpoint: 1199,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 3,
+//           infinite: true,
+//           dots: false
+//         }
+//       },
+//       {
+//         breakpoint: 991,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//           dots: false // add dots option
+//         }
+//       },
+//       {
+//         breakpoint: 767,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           autoplay: true,
+//           arrows: false,
+//           dots: false // add dots option
+//         }
+//       }
+//     ]
+//   });
+
+//   $('.autoplay').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     dots: true, // add dots option
+//     arrows: false, // remove arrows option
+//     responsive: [
+//       {
+//         breakpoint: 1199,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 3,
+//           infinite: true,
+//           dots: true
+//         }
+//       },
+//       {
+//         breakpoint: 991,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//           dots: true // add dots option
+//         }
+//       },
+//       {
+//         breakpoint: 767,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           dots: true // add dots option
+//         }
+//       }
+//     ]
+//   });
+
+//   $('.autoplay3').slick({
+//  slidesToShow: 3,
+//  slidesToScroll: 1,
+//  autoplay: true,
+//  autoplaySpeed: 2000,
+//  dots: false, // add dots option
+//  arrows: true, // remove arrows option
+//  prevArrow:'.priv_arrow2',
+//   nextArrow:'.next_arrow2',
+//  responsive: [
+//    {
+//      breakpoint: 1200,
+//      settings: {
+//        slidesToShow: 2,
+//        slidesToScroll: 1,
+//        infinite: true,
+//      }
+//    },
+//    {
+//      breakpoint: 767,
+//      settings: {
+//        slidesToShow: 1,
+//        slidesToScroll: 1,
+//      }
+//    }
+//  ]
+// });
+
+// $('.digitech-slider').slick({
+//   dots: false,
+//   arrows: true,
+//   prevArrow:'.priv_arrow4',
+//   nextArrow:'.next_arrow4',
+//   autoplay: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 5,
+//   slidesToScroll: 4,
+//   responsive: [
+//     {
+//       breakpoint: 1200,
+//       settings: {
+//         slidesToShow: 4,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 577,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//     // You can unslick at a given breakpoint now by adding:
+//     // settings: "unslick"
+//     // instead of a settings object
+//   ]
+// });
+
+// $(document).ready(function() {
+//   $(".toggler").click(function() {
+//     $(".sidebar, .content").addClass("show-sidebar");
+//   });
+//    $(".close").click(function() {
+//     $(".sidebar, .content").removeClass("show-sidebar");
+//   });
+
+//   // -------search-filter---
+//   $(".search-ic").click(function() {
+//     $(".search-popup, .content").addClass("show-search");
+//   });
+//    $(".close-search").click(function() {
+//     $(".search-popup, .content").removeClass("show-search");
+//   });
+// });
+
+
+
+
+
+
+$('.autoplay-2').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: false, // add dots option
+  arrows: true,
+  prevArrow:'.priv_arrow',
+  nextArrow:'.next_arrow',
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: false // add dots option
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        dots: false // add dots option
+      }
+    }
+  ]
+});
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true, // add dots option
+  arrows: false, // remove arrows option
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true // add dots option
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true // add dots option
+      }
+    }
+  ]
+});
+
+$('.autoplay3').slick({
+slidesToShow: 3,
+slidesToScroll: 1,
+autoplay: true,
+autoplaySpeed: 2000,
+dots: false, // add dots option
+arrows: true, // remove arrows option
+prevArrow:'.priv_arrow2',
+nextArrow:'.next_arrow2',
+responsive: [
+ {
+   breakpoint: 1200,
+   settings: {
+     slidesToShow: 2,
+     slidesToScroll: 1,
+     infinite: true,
+   }
+ },
+ {
+   breakpoint: 767,
+   settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+   }
+ }
+]
+});
+
+$('.autoplay-st').slick({
+slidesToShow: 3,
+slidesToScroll: 1,
+// autoplay: true,
+autoplaySpeed: 2000,
+dots: false, // add dots option
+arrows: true, // remove arrows option
+prevArrow:'.priv_arrow2',
+ nextArrow:'.next_arrow2',
+responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+    }
+  },
+  {
+    breakpoint: 568,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }
+  }
+]
+});
+$('.digitech-slider').slick({
+dots: false,
+arrows: true,
+prevArrow:'.priv_arrow4',
+nextArrow:'.next_arrow4',
+autoplay: true,
+infinite: false,
+speed: 300,
+slidesToShow: 5,
+slidesToScroll: 4,
+responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  },
+  {
+    breakpoint: 577,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+
+$(document).ready(function() {
+$(".toggler").click(function() {
+  $(".sidebar, .content").addClass("show-sidebar");
+});
+ $(".close").click(function() {
+  $(".sidebar, .content").removeClass("show-sidebar");
+});
+
+// -------search-filter---
+$(".search-ic").click(function() {
+  $(".search-popup, .content").addClass("show-search");
+});
+ $(".close-search").click(function() {
+  $(".search-popup, .content").removeClass("show-search");
+});
+});
+
+
+
